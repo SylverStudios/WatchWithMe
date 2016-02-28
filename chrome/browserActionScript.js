649c3a0c7c97f6f55a3464dfa82be46800f2afa3
Aaron Votre <avotre422@gmail.com>
2016-02-28 13:51:41 -0500

    Replaces browser action UI with new streamlined version.

---
+++ chrome/browserActionScript.js
@@ -0,0 +1,57 @@
+/*
+Browser Action:
+- Has a visible html element.
+- Runs everytime the popup is opened.
+- Has access to the Background Page and its functions.
+*/
+
+document.addEventListener('DOMContentLoaded', function() {
+  console.log("popup script is live.");
+
+  var background = chrome.extension.getBackgroundPage();
+
+  var action = {
+    CREATE : function() { background.sendCommand('CREATE'); populateHistory() },
+    JOIN : function() { background.sendCommand('JOIN'); populateHistory() },
+    FIND : function() { background.sendCommand('FIND'); populateHistory() },
+    PLAY : function() { background.sendCommand('PLAY'); populateHistory() },
+    PAUSE : function() { background.sendCommand('PAUSE'); populateHistory() },
+  }
+
+  var bindButtons = function() {
+    $('.create-button').click(action.CREATE);
+    $('.join-button').click(action.JOIN);
+    $('.find-button').click(action.FIND);
+    $('.play-button').click(action.PLAY);
+    $('.pause-button').click(action.PAUSE);
+  }
+
+  // Will need to use chrome-storage to make this work in real time vs. on page load
+  var populateHistory = function() {
+    var history = background.videoHistory.list;
+    var historyPageElement = $('#history-list');
+    $(historyPageElement).empty();
+
+    for (i = history.length; i >= 0; i--) {
+      history[i] ? $(historyPageElement).append('<li>'+history[i]+'</li>') : '';
+    }
+  }
+
+  var populateGroup = function() {
+    var groupPageElement = $('#group');
+    $(groupPageElement).text('Watching Solo');
+  }
+
+  var addUnloadListener = function() {
+    addEventListener("unload", function (event) {
+         background.sendCommand('POPUP_CLOSE');
+    }, true);
+  }
+
+  bindButtons();
+  populateHistory();
+  populateGroup();
+  addUnloadListener();
+  background.sendCommand('POPUP_OPEN');
+
+});
