2120279cbbb90a347ff7f60d276197327052b71c
Sam Roberts <samgqroberts@gmail.com>
2016-03-19 03:01:14 -0400

    tabs to spaces on ClientEvent

--- src/main/java/com/sylver/watchwithme/model/ClientEvent.java
+++ src/main/java/com/sylver/watchwithme/model/ClientEvent.java
@@ -12,53 +12,53 @@ import java.util.Optional;
  */
 public class ClientEvent {
 
-	public enum Type {
-		PLAY,
-		PAUSE,
-		BUFFERING_START,
-		BUFFERING_COMPLETE,
-		SEEK
-	}
+  public enum Type {
+    PLAY,
+    PAUSE,
+    BUFFERING_START,
+    BUFFERING_COMPLETE,
+    SEEK
+  }
 
-	private final Type type;
-	private final Optional<Double> time;
+  private final Type type;
+  private final Optional<Double> time;
 
-	@JsonCreator
-	public ClientEvent(
-		@JsonProperty(value = "type", required = true) final Type type,
-		@JsonProperty("time") final Double time
-	) {
-		this.type = type;
-		this.time = Optional.ofNullable(time);
-	}
+  @JsonCreator
+  public ClientEvent(
+    @JsonProperty(value = "type", required = true) final Type type,
+    @JsonProperty("time") final Double time
+  ) {
+    this.type = type;
+    this.time = Optional.ofNullable(time);
+  }
 
-	public Type getType() {
-		return type;
-	}
+  public Type getType() {
+    return type;
+  }
 
-	public Double getTime() {
-		return time.isPresent() ? time.get() : null;
-	}
+  public Double getTime() {
+    return time.isPresent() ? time.get() : null;
+  }
 
-	@Override
-	public boolean equals(Object o) {
-		if (this == o) return true;
-		if (o == null || getClass() != o.getClass()) return false;
-		ClientEvent that = (ClientEvent) o;
-		return type == that.type &&
-			Objects.equal(time, that.time);
-	}
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+    ClientEvent that = (ClientEvent) o;
+    return type == that.type &&
+      Objects.equal(time, that.time);
+  }
 
-	@Override
-	public int hashCode() {
-		return Objects.hashCode(type, time);
-	}
+  @Override
+  public int hashCode() {
+    return Objects.hashCode(type, time);
+  }
 
-	@Override
-	public String toString() {
-		return MoreObjects.toStringHelper(this)
-			.add("type", type)
-			.add("time", time)
-			.toString();
-	}
+  @Override
+  public String toString() {
+    return MoreObjects.toStringHelper(this)
+      .add("type", type)
+      .add("time", time)
+      .toString();
+  }
 }
