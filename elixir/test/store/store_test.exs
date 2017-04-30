7648dbcd3cfbe19c7e7ff7a5ee3e0fb177b65848
Aaron Votre <avotre422@gmail.com>
2017-04-30 10:44:55 -0400

    Improve store tests

--- elixir/test/store/store_test.exs
+++ elixir/test/store/store_test.exs
@@ -1,11 +1,30 @@
 defmodule StoreTest do
   use ExUnit.Case
+  alias Wwm.VideoState
+  alias Wwm.Store
 
-  test "caches and finds the correct data" do
-    assert Store.fetch(1, fn ->
-      %{id: 1, long_url: "http://www.example.com"}
-    end) == %{id: 1, long_url: "http://www.example.com"}
+  test "fetch will set default value if not found" do
+    default = default_video_state();
 
-    assert Store.fetch(1, fn -> "" end) == %{id: 1, long_url: "http://www.example.com"}
+    value_at_1 = Store.fetch(1, default)
+
+    assert default == value_at_1
+  end
+
+  test "fetch will return value stored" do
+    video_state = custom_video_state(true, 120, nil)
+
+    Store.set(5, video_state)
+
+    assert video_state == Store.fetch(5, default_video_state())
+  end
+
+# Private
+  defp default_video_state() do
+    %VideoState{is_playing: true, time: 100, last_action: nil}
+  end
+
+  defp custom_video_state(playing, time, last_action) do
+    %VideoState{is_playing: playing, time: time, last_action: last_action}
   end
 end
