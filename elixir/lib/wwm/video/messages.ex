194506833a4f8022e47cc2768b7cffd787afa2cc
Aaron Votre <avotre422@gmail.com>
2017-05-28 21:15:25 -0400

    Tabs to spaces in ex file

--- elixir/lib/wwm/video/messages.ex
+++ elixir/lib/wwm/video/messages.ex
@@ -1,27 +1,27 @@
 defmodule Wwm.Video.Messages do
-	@moduledoc """
-	This is only temporary, going to transition this
-	into an action that can be reduced
-	"""
+  @moduledoc """
+  This is only temporary, going to transition this
+  into an action that can be reduced
+  """
 
-	def message(username, content) do
-		%{sender: username, body: createBody(username, content)}
-	end
+  def message(username, content) do
+    %{sender: username, body: createBody(username, content)}
+  end
 
-	def joined(username) do
-		%{username: username, body: "#{username} joined!"}
-	end
+  def joined(username) do
+    %{username: username, body: "#{username} joined!"}
+  end
 
-	def welcome(username) do
-		%{username: username, body: "Welcome #{username}"}
-	end
+  def welcome(username) do
+    %{username: username, body: "Welcome #{username}"}
+  end
 
-	def exit(username) do
-		%{username: username, body: "#{username} left."}
-	end
+  def exit(username) do
+    %{username: username, body: "#{username} left."}
+  end
 
-# Helpers
-		defp createBody(username, body) do
-     "[#{username}] #{body}"
-  	end
+  # Helpers
+  defp createBody(username, body) do
+    "[#{username}] #{body}"
+  end
 end
