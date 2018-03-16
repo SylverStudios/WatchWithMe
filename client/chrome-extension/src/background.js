acedecc13563f01476bc1e0e00732f9fc1c700f0
samgqroberts <samgqroberts@gmail.com>
2018-03-16 00:00:45 -0400

    factor out non-environment-specific parts of background.js into AppController
    this modularity will lead to nicer testing

--- client/chrome-extension/src/background.js
+++ client/chrome-extension/src/background.js
@@ -1,76 +1,28 @@
-/**
- * Background script:
- * - Loaded behind the scenes.
- * - Exists as long as the browser is open.
- * - Can be accessed by other scipts in the extension.
- * - A middle man between the Browser Action and the Content Script.
- *
- * Responsibilities:
- * 1. handle messages from the content script
- * 2. Provide access to useful info to the browserAction
- * 3. handle messages from channel
- *  - Join messages go to video history
- *  - state updates go to content script
- */
-
 import Client from './client/Client';
 import AppState from './models/AppState';
 import { ChromeMessages } from './models/Constants';
+import AppController from './background/AppController';
 
-const init = () => {
-  console.log('init background');
-  // set up app state, to be shared with browserAction
-  window.state = new AppState();
-  let stateListener = () => {};
-  window.onStateChange = (callback) => {
-    stateListener = callback;
-  };
-  const updateState = (updater) => {
-    window.state = updater(window.state);
-    window.onStateChange(window.state);
-  };
-
-  // set up server client
-  const client = new Client('ws://localhost:4000/socket', 'user'); // TODO use real values for address and username
-
-  // set up messaging infrastructure
-  chrome.runtime.onMessage.addListener((message) => {
-    console.log('received message: ', message);
-    if (typeof message === 'string') {
-      switch (message) {
-        case 'FOUND_VIDEO':
-          updateState(s => s.set('connecting', true));
-          client.connect(() => {
-            updateState(s => s.set('connected', true).set('connecting', false));
-          }, () => {
-            updateState(s => s.set('connected', false).set('connecting', false).set('couldNotConnect', true));
-          });
-          return;
-        case 'NOT_EXACTLY_ONE_VIDEO':
-          updateState(s => s.set('pageIsInvalid', true));
-          return;
-      }
+/**
+ * Set up parameters to AppController based on environment
+ * Here environment is chrome extension and messaging needs to be plumbed a certain way
+ */
+const client = new Client('ws://localhost:4000/socket', 'user'); // TODO use real values for address and username
+const sendMessage = (message) => {
+  chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+    if (tabs[0]) {
+      chrome.tabs.sendMessage(tabs[0].id, message);
+    } else {
+      // TODO call an onError callback denoting that the tab can't be found
+      console.error('could not find active tab');
     }
   });
-  const sendMessageToContentScript = (message) => {
-    console.log('sendMessageToContentScript:', message);
-    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-      console.log('found ' + tabs.length + ' tabs');
-      if (tabs[0]) {
-        chrome.tabs.sendMessage(tabs[0].id, message);
-      } else {
-        console.log('could not find active tab');
-      }
-    });
-  }
-
-  // set up actions that the browserAction popup can perform
-  window.browserActionActions = {
-    attemptConnect: () => {
-      console.log('attemptConnect')
-      sendMessageToContentScript(ChromeMessages.FIND_NEW_VIDEO_COMMAND);
-    },
-  };
 }
+const onMessage = fn => chrome.runtime.onMessage.addListener(fn);
 
-init();
+const appController = new AppController({ client, sendMessage, onMessage });
+
+// make appController information available on the window, so browserAction can access it
+window.state = appController.state;
+window.onStateChange = appController.onStateChange;
+window.browserActionActions = appController.browserActionActions;
--- client/chrome-extension/src/browserAction/PopupContent.jsx
+++ client/chrome-extension/src/browserAction/PopupContent.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 
 const PopupContent = ({ connected, pageIsInvalid, couldNotConnect, onConnectClick }) => {
-  console.log(onConnectClick);
   return (
     <div className="popup-content">
       <h3 className="popup-title">Watch With Me</h3>
@@ -10,7 +9,7 @@ const PopupContent = ({ connected, pageIsInvalid, couldNotConnect, onConnectClic
         <span className="invalid-page">Cannot connect - there is not exactly one video on this page</span>
       ) : null}
       {couldNotConnect ? (
-        <span className="invalid-page">Cannot connect - could not establish connection with server</span>
+        <span className="invalid-page">Could not establish connection with server</span>
       ) : null}
     </div>
   );
