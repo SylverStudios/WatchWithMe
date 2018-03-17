db209cb5bcedeef7c6f4abf1e1a6c98452d37c64
samgqroberts <samgqroberts@gmail.com>
2018-03-17 16:30:23 -0400

    add mocha, chai and simple test

---
+++ client/chrome-extension/src/sample.test.js
@@ -0,0 +1,10 @@
+/* globals describe, it */
+/* eslint-disable no-unused-expressions */
+
+const { expect } = require('chai');
+
+describe('sample test', () => {
+  it('works', async () => {
+    expect(true).to.be.true;
+  });
+});
