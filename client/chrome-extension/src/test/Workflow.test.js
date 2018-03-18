0512db0c8de6b4843dc3b9e5f1a716fa939b64aa
samgqroberts <samgqroberts@gmail.com>
2018-03-18 12:56:22 -0400

    next leg of WorkflowTest - mount PopupContent, observe "Connecting..."

--- client/chrome-extension/src/test/Workflow.test.js
+++ client/chrome-extension/src/test/Workflow.test.js
@@ -1,6 +1,7 @@
-/* eslint-disable no-unused-expressions one-var no-new */
+/* eslint-disable no-unused-expressions one-var no-new, one-var-declaration-per-line */
 
 import React from 'react';
+import { expect } from 'chai';
 import { JSDOM } from 'jsdom';
 import { mount, configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
@@ -13,18 +14,19 @@ import MockClient from '../client/MockClient';
 configure({ adapter: new Adapter() });
 
 describe('The Entire App', () => {
+  let appController, pageWatcher, popupContent; // eslint-disable-line no-unused-vars
   it('can be initialized in a test environment', () => {
     const client = new MockClient();
 
     let onMessageToAppController = () => {};
     let onMessageToPageWatcher = () => {};
-    new AppController({
+    appController = new AppController({
       client,
       sendMessage: (message) => onMessageToPageWatcher(message),
       onMessage: (callback) => { onMessageToAppController = callback; },
     });
-    new PageWatcher({
-      dom: new JSDOM('<!DOCTYPE html><p>Hello world</p>').window.document,
+    pageWatcher = new PageWatcher({
+      dom: new JSDOM('<!DOCTYPE html><video />').window.document,
       sendMessage: (message) => onMessageToAppController(message),
       onMessage: (callback) => { onMessageToPageWatcher = callback; },
     });
@@ -32,6 +34,20 @@ describe('The Entire App', () => {
     // enzyme needs globally available document to attach mounted react components to.
     // JSDOM allows us to avoid this document colliding with the document provided to PageWatcher.
     global.document = new JSDOM().window.document;
-    mount(<PopupContent />);
+
+    // set up popup react component
+    const getPopupProps = () => {
+      return {
+        ...appController.state.toObject(),
+        ...appController.browserActionActions,
+      };
+    };
+    popupContent = mount(<PopupContent {...getPopupProps()} />);
+    appController.onStateChange(() => popupContent.setProps(getPopupProps()));
+  });
+
+  it('attempts to connect when connect button is clicked', () => {
+    popupContent.find('button').simulate('click');
+    expect(popupContent.someWhere(n => n.text().includes('Connecting...'))).to.be.true;
   });
 });
